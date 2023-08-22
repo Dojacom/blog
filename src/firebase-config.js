92e8ea3f2806ad27b73234078abd647d88e837b0
OLADOJA💙 <olansileismail1@gmail.com>
2023-08-22 02:39:06 +0100

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBimM8APx_xcNJc-jZlYT5sE9MEpN8pUs",
+  authDomain: "doja-blog.firebaseapp.com",
+  projectId: "doja-blog",
+  storageBucket: "doja-blog.appspot.com",
+  messagingSenderId: "447190446092",
+  appId: "1:447190446092:web:4d76ae248b0b1a413dbe29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider()
